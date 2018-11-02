b068d2f32320480f2def60877935497495dcf50e
Shawna <shawna.donnelly@nwyc.com>
2018-11-02 16:04:31 -0500

    write code and tests for successReducer

--- src/reducers/successReducer.js
+++ src/reducers/successReducer.js
@@ -1,7 +1,15 @@
+import { actionTypes } from '../actions';
 /**
  * 
  * 
  */
-export default(state, action) => {
+export default(state=false, action) => {
+  switch(action.type){
+    case(actionTypes.CORRECT_GUESS):
+      return true;
+      break;
+    default:
+      return state;
+  }
   return null;
 }
