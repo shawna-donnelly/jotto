3eae227418b799da447edced6cd99ad4ffde532d
Shawna <shawna.donnelly@nwyc.com>
2018-11-05 12:55:26 -0600

    Set up guessWords tests

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,16 @@
 export const actionTypes = {
   CORRECT_GUESS: 'CORRECT_GUESS',
+  GUESS_WORD: 'GUESS_WORD',
 };
 
-export function correctGuess(){
-  return {type: actionTypes.CORRECT_GUESS};
+/**
+ * Returns Redux Thunk function that dispatches GUESS_WORD action
+ * and (conditionally) CORRECT_GUESS action
+ * @param {string} guessedWord - Guessed word.
+ * @returns {function} - Redux Thunk function
+ */
+export const guessWord = (guessedWord) => {
+  return function(dispatch, getState){
+
+  }
 }
--- src/reducers/successReducer.js
+++ src/reducers/successReducer.js
@@ -7,9 +7,7 @@ export default(state=false, action) => {
   switch(action.type){
     case(actionTypes.CORRECT_GUESS):
       return true;
-      break;
     default:
       return state;
   }
-  return null;
 }
